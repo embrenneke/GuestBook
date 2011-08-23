c84f4e32d2575cdab706f3daaeb46ca92e83c9b0
Matt Brenneke <brenneke@gmail.com>
2011-08-23 12:20:49 -0500

    Initial Commit

---
+++ GuestBook/RootViewController.h
@@ -0,0 +1,20 @@
+//
+//  RootViewController.h
+//  GuestBook
+//
+//  Created by Matt Brenneke on 8/23/11.
+//  Copyright 2011 UnspunProductions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface RootViewController : UITableViewController <NSFetchedResultsControllerDelegate> {
+
+}
+
+@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+
+@end
---
+++ GuestBookTests/GuestBookTests.h
@@ -0,0 +1,17 @@
+//
+//  GuestBookTests.h
+//  GuestBookTests
+//
+//  Created by Matt Brenneke on 8/23/11.
+//  Copyright 2011 UnspunProductions. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface GuestBookTests : SenTestCase {
+@private
+    
+}
+
+@end
