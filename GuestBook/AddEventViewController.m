4b2da22cc2d65b3320a0ad721440ebcecdf89e41
Emily Brenneke <brenneke@gmail.com>
2011-08-29 22:16:41 -0500

    fill out add event view, tie it's data to db.  need to fix date picker location/size

--- GuestBook/AddEventViewController.m
+++ GuestBook/AddEventViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "AddEventViewController.h"
-
+#import "GuestBookAppDelegate.h"
+#import "Event.h"
 
 @implementation AddEventViewController
+
+@synthesize fetchedResultsController;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -18,6 +21,34 @@
         // Custom initialization
     }
     return self;
+}
+
+- (IBAction)createEvent:(id)sender
+{
+    // Create a new instance of the entity managed by the fetched results controller.
+    NSManagedObjectContext *context = [self.fetchedResultsController managedObjectContext];
+    NSEntityDescription *entity = [[self.fetchedResultsController fetchRequest] entity];
+    Event* event = [NSEntityDescription insertNewObjectForEntityForName:[entity name] inManagedObjectContext:context];
+    
+    [event setName:[name text]];
+    [event setTime:[datePicker date]];
+    GuestBookAppDelegate *appDelegate = (GuestBookAppDelegate *)[[UIApplication sharedApplication] delegate];
+    [event setUuid:[appDelegate generateUuidString]];
+    
+    // Save the context.
+    NSError *error = nil;
+    if (![context save:&error])
+    {
+        /*
+         Replace this implementation with code to handle the error appropriately.
+         
+         abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. If it is not possible to recover from the error, display an alert panel that instructs the user to quit the application by pressing the Home button.
+         */
+        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+        abort();
+    }
+    
+    [[self navigationController] popViewControllerAnimated:YES];
 }
 
 - (void)dealloc
