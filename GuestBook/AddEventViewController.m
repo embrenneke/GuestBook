605d1beee4df047e72a5e6f38c39e99806dcfac3
Emily Brenneke <brenneke@gmail.com>
2011-08-30 20:02:13 -0500

    set current date to default date in datePicker.  Move picker right until it was at least on-screen

--- GuestBook/AddEventViewController.m
+++ GuestBook/AddEventViewController.m
@@ -72,6 +72,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     self.contentSizeForViewInPopover = CGSizeMake(700.0, 250.0);
+    [datePicker setDate:[NSDate date]];
 }
 
 - (void)viewDidUnload
