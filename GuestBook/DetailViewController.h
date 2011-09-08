9d4ca73d9fc960f43d15936a7ebc55faf0b72f3c
Emily Brenneke <brenneke@gmail.com>
2011-09-07 19:18:01 -0500

    add automatic video playback to detail view when selecting a signature with video

--- GuestBook/DetailViewController.h
+++ GuestBook/DetailViewController.h
@@ -7,13 +7,17 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MediaPlayer/MPMoviePlayerController.h>
 #import "Signature.h"
 
 @interface DetailViewController : UIViewController {
     IBOutlet UIImageView *imageView;
     IBOutlet UITextView  *messageView;
     Signature            *signature;
+    MPMoviePlayerController *moviePlayer;
 }
 
 @property (nonatomic, retain) Signature *signature;
+@property (nonatomic, retain) MPMoviePlayerController *moviePlayer;
+
 @end
