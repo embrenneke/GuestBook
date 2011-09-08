780a0b90a276a057e8b1b9ce0f67acfeb90ea214
Matt Brenneke <brenneke@gmail.com>
2011-09-07 19:18:01 -0500

    add automatic video playback to detail view when selecting a signature with video

--- GuestBook/DetailViewController.m
+++ GuestBook/DetailViewController.m
@@ -7,11 +7,13 @@
 //
 
 #import "DetailViewController.h"
+#import <MediaPlayer/MPMoviePlayerController.h>
 
 
 @implementation DetailViewController
 
 @synthesize signature;
+@synthesize moviePlayer;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -43,12 +45,27 @@
     // Do any additional setup after loading the view from its nib.
     if(signature.mediaPath)
     {
-        [imageView setImage:[UIImage imageWithContentsOfFile:[signature mediaPath]]];
+        if([signature.mediaPath hasSuffix:@"jpg"])
+        {
+           [imageView setImage:[UIImage imageWithContentsOfFile:[signature mediaPath]]];
+
+        }
+        else if([signature.mediaPath hasSuffix:@"mp4"])
+        {
+            MPMoviePlayerController *player = [[MPMoviePlayerController alloc] initWithContentURL:[NSURL fileURLWithPath:signature.mediaPath]];
+            moviePlayer = player;
+            player.view.frame = imageView.frame;
+            [self.view addSubview:player.view];
+            [player play];
+        }
     }
     if(signature.message)
     {
         messageView.text = signature.message;
     }
+    
+    [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(detectOrientation) name:@"UIDeviceOrientationDidChangeNotification" object:nil];
 }
 
 - (void)viewDidUnload
@@ -62,6 +79,13 @@
 {
     // Return YES for supported orientations
 	return YES;
+}
+
+-(void) detectOrientation {
+    if(moviePlayer)
+    {
+        moviePlayer.view.frame = imageView.frame;
+    }
 }
 
 @end
