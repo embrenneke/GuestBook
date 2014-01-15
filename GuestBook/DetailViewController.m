01942808679000d43b11614935bb34633f6cb232
Emily Brenneke <brenneke@gmail.com>
2014-01-14 19:59:17 -0600

    more general cleanup

--- GuestBook/DetailViewController.m
+++ GuestBook/DetailViewController.m
@@ -23,15 +23,6 @@
 
 @implementation DetailViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)dealloc
 {
     [self.moviePlayer stop];
@@ -44,59 +35,49 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
-    if(self.signature.mediaPath)
-    {
+
+    if(self.signature.mediaPath) {
         GuestBookAppDelegate *appDelegate = (GuestBookAppDelegate *)[[UIApplication sharedApplication] delegate];
         NSString* filePath = [[NSString alloc] initWithFormat:@"%@", [[[appDelegate applicationLibraryDirectory] URLByAppendingPathComponent:self.signature.mediaPath] path]];
         
-        if([filePath hasSuffix:@"jpg"])
-        {
+        if([filePath hasSuffix:@"jpg"]) {
            [self.imageView setImage:[UIImage imageWithContentsOfFile:filePath]];
-        }
-        else if([filePath hasSuffix:@"mp4"])
-        {
+        } else if([filePath hasSuffix:@"mp4"]) {
             MPMoviePlayerController *player = [[MPMoviePlayerController alloc] initWithContentURL:[NSURL fileURLWithPath:filePath]];
             [player stop];
             self.moviePlayer = player;
             self.moviePlayer.view.frame = self.imageView.frame;
             [self.view addSubview:self.moviePlayer.view];
-            [self.moviePlayer play];
-            [self performSelector:@selector(detectOrientation) withObject:nil afterDelay:0.2];
         }
     }
-    else
-    {
-        NSString* pathToImageFile = [[NSBundle mainBundle] pathForResource:@"no-media" ofType:@"png"];
-        [self.imageView setImage:[UIImage imageWithContentsOfFile:pathToImageFile]];
+    else {
+        [self.imageView setImage:[UIImage imageNamed:@"no-media"]];
     }
     
-    if(self.signature.message)
-    {
-        self.messageView.text = self.signature.message;
+    self.messageView.text = self.signature.message;
+    self.titleView.text = self.signature.name;
+
+    if ([self respondsToSelector:@selector(edgesForExtendedLayout)]) {
+        [self setEdgesForExtendedLayout:UIRectEdgeNone];
     }
-    else
-    {
-        self.messageView.text = nil;
-    }
-    
-    if(self.signature.message)
-    {
-        self.titleView.text = self.signature.name;
-    }
-    else
-    {
-        self.titleView.text = nil;
-    }
-    
+
     [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(detectOrientation) name:@"UIDeviceOrientationDidChangeNotification" object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(detectOrientation)
+                                                 name:@"UIDeviceOrientationDidChangeNotification"
+                                               object:nil];
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:YES];
     [self.navigationController setNavigationBarHidden:NO animated:animated];
+
+    [self detectOrientation];
+
+    if (self.moviePlayer) {
+        [self.moviePlayer play];
+    }
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
@@ -105,9 +86,9 @@
 	return YES;
 }
 
--(void) detectOrientation {
-    if(self.moviePlayer)
-    {
+-(void) detectOrientation
+{
+    if(self.moviePlayer) {
         self.moviePlayer.view.frame = self.imageView.frame;
     }
 }
