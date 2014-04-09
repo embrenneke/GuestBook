e293b72e1022e77bcfdffa9dba8ba94375a058a0
Emily Brenneke <brenneke@gmail.com>
2014-04-08 23:08:21 -0500

    update copyrights on a couple files

--- GuestBook/EventListController.m
+++ GuestBook/EventListController.m
@@ -3,7 +3,7 @@
 //  GuestBook
 //
 //  Created by Emily Brenneke on 8/23/11.
-//  Copyright 2013 Emily Brenneke. All rights reserved.
+//  Copyright 2014 Emily Brenneke. All rights reserved.
 //  Release under the MIT license.  See the LICENSE file in top directory of this project.
 //
 
